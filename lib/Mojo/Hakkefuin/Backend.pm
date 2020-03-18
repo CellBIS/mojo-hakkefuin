ea50297616ec99f8cfbb7b6d8126fcebf1685004
Achmad Yusri Afandi <linuxer08@gmail.com>
2020-03-18 21:08:23 +0800

    update base backend modules - #5 & #1

--- lib/Mojo/Hakkefuin/Backend.pm
+++ lib/Mojo/Hakkefuin/Backend.pm
@@ -4,12 +4,12 @@ use Mojo::Base -base;
 use Carp 'croak';
 use Mojo::Hakkefuin::Utils;
 
+has 'dsn';
 has 'dir';
-has 'app';
 has mhf_util => 'Mojo::Hakkefuin::Utils';
 
 # table structure
-has table_name  => 'mojo_simple_auth';
+has table_name  => 'mojo_hakkefuin';
 has id          => 'id_auth';
 has identify    => 'identify';
 has cookie      => 'cookie';
@@ -17,17 +17,21 @@ has csrf        => 'csrf';
 has create_date => 'create_date';
 has expire_date => 'expire_date';
 has cookie_lock => 'cookie_lock';
-has lock        => 'lock';
+has lock        => 'lock_state';
 
+sub table_query { croak 'Method "table_query" not implemented by subclass' }
 sub check_table { croak 'Method "check_table" not implemented by subclass' }
 sub create_table { croak 'Method "create_table" not implemented by subclass' }
-sub table_query { croak 'Method "table_query" not implemented by subclass' }
+sub drop_table { croak 'Method "drop_table" not implemented by subclass' }
+sub empty_table { croak 'Method "empty_table" not implemented by subclass' }
 
 sub create { croak 'Method "create" not implemented by subclass' }
 sub read { croak 'Method "read" not implemented by subclass' }
 sub update { croak 'Method "update" not implemented by subclass' }
 sub update_csrf { croak 'Method "update_csrf" not implemented by subclass' }
 sub update_cookie { croak 'Method "update_cookie" not implemented by subclass' }
+sub update_cookie_lock { croak 'Method "update_lock_state" not implemented by subclass' }
+sub update_lock_state { croak 'Method "update_lock_state" not implemented by subclass' }
 sub delete { croak 'Method "delete" not implemented by subclass' }
 sub check { croak 'Method "check" not implemented by subclass' }
 
@@ -44,14 +48,18 @@ Mojo::Hakkefuin::Backend - Backend base class
   package Mojo::Hakkefuin::Backend::MyBackend;
   use Mojo::Base 'Mojo::Hakkefuin::Backend';
   
+  sub table_query { ... }
   sub check_table { ... }
   sub create_table { ... }
-  sub table_query { ... }
+  sub drop_table { ... }
+  sub empty_table { ... }
   sub create { ... }
   sub read { ... }
   sub update { ... }
   sub update_csrf { ... }
   sub update_cookie { ... }
+  sub update_cookie_lock { ... }
+  sub update_lock_state { ... }
   sub delete { ... }
   sub check { ... }
 
